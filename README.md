d6e3e1f207adc0e9ce73c96046a46a8326f44774
2beast101 <bradendavidc@yahoo.com>
2023-04-02 17:28:10 +0000

    Updated README, and commenting code.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # producer-consumer
+The producer produces items, 1-100, and places them in the buffer.
+When the consumer is ready, it takes an item out of the buffer, and consumes it.
+This process works even if the producer is faster than the consumer, and 
+if the consumer is faster than the producer.
+
+Compiling the program: clang++ -pthread producer-consumer.cpp -o producer-consumer
+
+Running the program: ./producer-consumer
+
+Output:
--- producer-consumer.cpp
+++ producer-consumer.cpp
@@ -12,17 +12,20 @@ int count = 0;
 
 const int THREAD_NUM = 2;
 pthread_mutex_t mutexBuffer;
-
 sem_t semEmpty, semFull;
+
+
 
 void* producer(void* args) {
 	while (1){
 		int x = rand() % 100;
-		sleep(1);
+        sleep(1);
 		sem_wait(&semEmpty);
 		pthread_mutex_lock(&mutexBuffer);
 		
 		buffer[count] = x;
+        std::cout << "Produced: " << x << " at index: " << count << std::endl;
+
 		count++;
 		
 		
@@ -41,14 +44,13 @@ void* consumer(void* args) {
 		
 		
 		y = buffer[count - 1];
+        std::cout << "Consumed: " << y << " at index: " << count << std::endl;
 		count--;
 		
 
 		
 
 		// consume
-		std::cout << "Got " << y << std::endl;
-		// sleep(1);
 		pthread_mutex_unlock(&mutexBuffer);
 		sem_post(&semEmpty);
 
@@ -59,19 +61,21 @@ void* consumer(void* args) {
 
 
 int main(int argv, char* argc[]){
-	srand(time(NULL));
 	pthread_t th[THREAD_NUM];
 	pthread_mutex_init(&mutexBuffer, NULL);
-	sem_init(&semEmpty, 0, 2);
-	sem_init(&semFull, 0, 0);
+
+	sem_init(&semEmpty, 0, 2); // initialize semEmpty,  with value 2, as there are 2 empty slots
+	sem_init(&semFull, 0, 0); // intialize semFull, with 0, as there are 0 full slots
+
 	int i;
+
 	for (i = 0; i < THREAD_NUM; ++i ) {
 		if (i % 2 == 0) {
-			if (pthread_create(&th[i], NULL, &producer, NULL) != 0) {
+			if (pthread_create(&th[i], NULL, &producer, NULL) != 0) { // Makes sure producer thread is created correctly
 				perror("Failed to create thread");
 			}
 		} else {
-			if (pthread_create(&th[i], NULL, &consumer, NULL) != 0) {
+			if (pthread_create(&th[i], NULL, &consumer, NULL) != 0) { // Makes sure consumer thread is created correctly
 				perror("Failed to create thread");
 			}
 		}
